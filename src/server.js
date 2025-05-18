6130844b8a04abfd8060bb4a68f5856a12b90150
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-05-18 18:16:48 -0300

    FEAT: adição das rotas de Despesas e Receitas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors"
+import receitaRouter from "./routers/receitaRouter.js"
+import despesaRouter from "./routers/despesaRouter.js"
+
+
 const app = express();
 const port = 3000;
 
-app.get("/", (_, res) => {
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/receitas", receitaRouter)
+app.use("/despesas", despesaRouter)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
