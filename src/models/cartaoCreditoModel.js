7846083edd28bf7511db33d15f1b19793e93e3e6
emersonsoasilva <emersonsoares141@gmail.com>
2025-05-20 21:02:57 -0300

    Implentação model - Cartão de Crédito [Finalizada]

--- src/models/cartaoCreditoModel.js
+++ src/models/cartaoCreditoModel.js
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 import { PrismaClient } from "@prisma/client";
-import { despesasSchema, receitasSchema } from "../models";
+import { despesasSchema, receitasSchema, userSchema } from "../models";
 
 const prisma = new PrismaClient();
 
-const cartaoCreditoSchema = z.object({
+const cartaosSchema = z.object({
     nome_cartao: z
     .string({ invalid_type_error: "Nome do cartão de crédito deve ser texto"})
     .max(255, "Máximo de 255 caracteres"),
@@ -23,10 +23,21 @@ const cartaoCreditoSchema = z.object({
     .number({ invalid_type_error: "ID da conta deve ser número" }).nonnegative()
     .int("ID do usuário dever ser inteiro"),
 
+    users: userSchema,
+
     despesas: despesasSchema,
 
     receitas: receitasSchema,
 });
+
+export const cartaoValidator = (user, partial = null) => {
+    if (partial) {
+        return cartaosSchema.partial(partial).safeParse(user)
+    }
+    else {
+        return cartaosSchema.safeParse(user)
+    }
+};
 
 export async function create(cartao) {
     return await prisma.cartaos.create({
@@ -36,9 +47,37 @@ export async function create(cartao) {
             icone_cartao: true;
             limite_cartao: true,
             id_user: true,
+            users: true,
             despesas: true,
             receitas: true
         }
     })
     
+};
+
+export async function findAll() {
+  return await prisma.cartaos.findMany();
 }
+
+
+export async function findById(id) {
+  return await prisma.cartaos.findUnique({
+    where: { id },
+  });
+}
+
+
+export async function update(id, data) {
+  return await prisma.cartaos.update({
+    where: { id },
+    data
+  });
+}
+
+
+export async function remove(id) {
+  return await prisma.cartaos.delete({
+    where: { id },
+  });
+}
+
