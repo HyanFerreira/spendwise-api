56c1e6a72b0a0087480ea4274e5e85e4b8a15b30
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-05-27 21:33:39 -0300

    FEAT: server.js updated

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ import receitaRouter from "./routers/receitaRouter.js";
 import despesaRouter from "./routers/despesaRouter.js";
 import userRouter from "./routers/userRouter.js";
 import contaRouter from "./routers/contaRouter.js";
-import cartaoCreditoRouter from "./routers/cartaoCreditoRouter.js"
+import cartaoCreditoRouter from "./routers/cartaoCreditoRouter.js";
+import categoriasRouter from "./routers/categoriasRouter.js";
 
 const app = express();
 const port = 3000;
@@ -21,6 +22,7 @@ app.use("/despesas", despesaRouter);
 app.use("/users", userRouter);
 app.use("/conta", contaRouter);
 app.use("/cartao", cartaoCreditoRouter);
+app.use("/categorias", categoriasRouter);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
