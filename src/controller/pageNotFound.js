5e173371681c8e0bef8e24f756227649bb17f1d0
Robert-Cortez-Rudi <robertcortezrudi@gmail.com>
2025-06-03 20:37:05 -0300

    pageNotFound deleted

--- src/controller/pageNotFound.js
+++
@@ -1,5 +0,0 @@
-export const pageNotFound = (req, res) => {
-    return res.status(404).json({
-        message: "Erro! A sua página não foi encontrada! Entre em uma página que exista no sistema!"
-    })
-}
